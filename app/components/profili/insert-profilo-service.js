563d635fdaaa98edd3dcbf5b97056fa1bfbb12bc
MatteoGiambiasi <mgiambia@hotmail.it>
2017-06-17 18:22:54 +0200

    registrazione

--- app/components/profili/insert-profilo-service.js
+++ app/components/profili/insert-profilo-service.js
@@ -3,31 +3,36 @@
 //The service implemented in this module will save information about pizzas
 angular.module('myApp.profilo.insertProfiloService', [])
 
-    .factory('InsertProfiloService', function($firebaseArray) {
-        var NewProfiloService = {
-            insertNewProfilo: function (cognome, email, id_profilo, img_descr, img_profilo, nome, prof_pass, prof_user) {
-                //add the user to list of users and set the logged value to true
-                var ref = firebase.database().ref().child("profili");
-                // create a synchronized array
-                return $firebaseArray(ref).$add({
-                    cognome : cognome,
-                    email : email,
-                    id_profilo : id_profilo,
-                    img_descr : img_descr,
-                    img_profilo : img_profilo,
-                    nome : nome,
-                    prof_pass : prof_pass,
-                    prof_user : prof_user
-                });
-            },
-            updateEvento: function (id_profilo) {
+    .factory('Users', function($firebaseArray) {
+        return {
+            registerLogin: function (id_profilo, email) {
                 //add the user to list of users and set the logged value to true
                 var ref = firebase.database().ref().child("profili").child(id_profilo);
                 // create a synchronized array
                 ref.update({
-                    id: id_profilo
+                    email: email,
+                    logged: true
+                });
+            },
+            registerLogout: function (id_profilo)
+            {
+                var ref = firebase.database().ref().child("profili").child(id_profilo);
+                // create a synchronized array
+                ref.update({
+                    logged: false
+                });
+            },
+            registerNewUserInfo: function (id_profilo, email, nome, cognome, prof_user, tipo) {
+                //add the user to list of users and set the logged value to true
+                var ref = firebase.database().ref().child("profili").child(id_profilo);
+                // create a synchronized array
+                ref.set({
+                    email : email,
+                    nome: nome,
+                    cognome : cognome,
+                    prof_user : prof_user,
+                    tipo: tipo
                 });
             }
         };
-        return NewProfiloService;
     });
